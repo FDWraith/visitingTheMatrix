1dcf1f4cf74efc4501fe06286e9533aa1cc3e5a6
Kevin Zhang <kevinzhang3702@gmail.com>
2017-02-18 12:21:58 -0500

    I think I finished matrix.py

--- matrix.py
+++ matrix.py
@@ -19,12 +19,22 @@ def ident( matrix ):
                 matrix[row][col] = 0
 
 def scalar_mult( matrix, s ):
-    pass
+    for row in range( matrix.length ):
+        for col in range( matrix[row].length ):
+            matrix[row][col] = matrix[row][col] * s;
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
-
+    for col in range(m2[0].length):
+        for row in range(m2.length):
+            m2[row][col] = multRowCol( m1[row], m2, col)
+    
+    
+def multRowCol( m1Row, m2, m2Col ):
+    sum = 0;
+    for index in m1Row.length:
+        sum += m1Row[index] * m2[index][m2Col];
+    return sum;
 
 def new_matrix(rows = 4, cols = 4):
     m = []
