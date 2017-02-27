c1de9b442e4758cf25e0ff93d94540a2bf03b20c
Kevin Zhang <kevinzhang3702@gmail.com>
2017-02-26 20:43:55 -0500

    some mods

--- main.py
+++ main.py
@@ -2,39 +2,42 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 0, 255, 255 ]
 matrix = new_matrix()
 
 print_matrix(matrix)
 
-add_edge( matrix, 0, 250, 0, 500, 250, 0)
-add_edge( matrix, 250, 0, 0, 250, 500, 0)
+#add_edge( matrix, 0, 250, 0, 500, 250, 0)
+#add_edge( matrix, 250, 0, 0, 250, 500, 0)
 
-print_matrix(matrix) 
+for i in range(0, 500, 8):
+    #add_edge( matrix, 0 + i, 0, 0, 500, 0 + i, 0)
+    add_edge( matrix, 250, 0 + i/2, 0, 250 + i/2, 250, 0 )
+    add_edge( matrix, 250, 0 + i/2, 0, 250 - i/2, 250, 0 )
+    
+    add_edge( matrix, 250 + i/2, 250, 0, 500, 250 + i/2, 0 )
+    add_edge( matrix, 0, 500 - i / 2, 0, 0 + i / 2, 250, 0 )
+#print_matrix(matrix) 
 
 draw_lines( matrix, screen, color )
 
-identMatrix = new_matrix()
-ident( identMatrix )
 
+
+identMatrix = new_matrix()
 print_matrix( identMatrix )
 
-newMatrix = new_matrix()
-ident( newMatrix )
-scalar_mult( newMatrix, 7 )
+ident( identMatrix )
+print_matrix( identMatrix )
+scalar_mult( identMatrix, 9 )
+print_matrix( identMatrix )
 
-print_matrix( newMatrix )
-
-matrix_mult( identMatrix, newMatrix )
-
-print_matrix( newMatrix )
+scalar_mult( matrix, 7 )
 
 matrix_mult( identMatrix, matrix )
 
-print_matrix( matrix )
+#color = [ 255, 0 , 0]
 
-matrix_mult( newMatrix, matrix )
+#draw_lines( matrix, screen, color )
 
-print_matrix( matrix )
 
 display(screen)
