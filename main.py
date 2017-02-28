8b99f1396639cba385b9ec29d1b7f74cbe949383
Kevin Zhang <kevinzhang3702@gmail.com>
2017-02-28 11:33:20 -0500

    more tadas

--- main.py
+++ main.py
@@ -3,12 +3,41 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 255 ]
-matrix = new_matrix()
-
-print_matrix(matrix)
 
 #add_edge( matrix, 0, 250, 0, 500, 250, 0)
 #add_edge( matrix, 250, 0, 0, 250, 500, 0)
+
+print "\nEmpty Matrix\n"
+emptyMatrix = new_matrix()
+print_matrix(emptyMatrix)
+
+print "\nTesting Identity Matrix\n" 
+identMatrix = new_matrix()
+#print_matrix( identMatrix )
+ident( identMatrix )
+print_matrix( identMatrix )
+
+print "\nTesting Scalar Multiplication\n"
+scalar_mult( identMatrix, 9 )
+print_matrix( identMatrix )
+
+print "\nCreating New Matrix and Adding Edge [10, 10, 0], [15, 15, 0]\n"
+testMatrix = new_matrix( 0, 4)
+add_edge( testMatrix, 10, 10, 0, 15, 15, 0)
+print_matrix( testMatrix )
+
+print "\nTesting Matrix Multiplication with Scalared Identity Matrix\n"
+matrix_mult( identMatrix, testMatrix )
+print_matrix( testMatrix )
+#scalar_mult( matrix, 7 )
+
+#matrix_mult( identMatrix, matrix )
+
+#color = [ 255, 0 , 0]
+
+#draw_lines( matrix, screen, color )
+
+matrix = new_matrix( 0, 4)
 
 for i in range(0, 500, 8):
     #add_edge( matrix, 0 + i, 0, 0, 500, 0 + i, 0)
@@ -20,34 +49,5 @@ for i in range(0, 500, 8):
 #print_matrix(matrix) 
 
 draw_lines( matrix, screen, color )
-
-print "Testing Identity Matrix\n" 
-identMatrix = new_matrix()
-#print_matrix( identMatrix )
-ident( identMatrix )
-print_matrix( identMatrix )
-
-print "\nTesting Scalar Multiplication\n"
-scalar_mult( identMatrix, 9 )
-print_matrix( identMatrix )
-
-print "\nCreating New Matrix and Adding Edge\n"
-testMatrix = new_matrix()
-add_edge( testMatrix, 120, 120, 0, 15, 15, 0)
-print_matrix( testMatrix )
-
-print "\nTesting Matrix Multiplication with a Fresh Identity Matrix"
-secondIdent = new_matrix()
-ident( secondIdent )
-matrix_mult( secondIdent, testMatrix )
-print_matrix( testMatrix )
-#scalar_mult( matrix, 7 )
-
-#matrix_mult( identMatrix, matrix )
-
-#color = [ 255, 0 , 0]
-
-#draw_lines( matrix, screen, color )
-
 
 display(screen)
